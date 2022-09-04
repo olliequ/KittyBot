abd9c79278d8829e0c366e64ac15d198962e6e8c
alex <>
2022-09-04 21:46:55 +1000

    duplicate messages stored as hash and deleted if sent again
    this is a vague attempt at preventing low-effort text, although in
    this case there are some caveats (like allowing messages of <= 4)

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 def cursor():
     return conn.cursor()
@@ -12,7 +13,13 @@ def start():
     c.execute("CREATE UNIQUE INDEX IF NOT EXISTS emoji_counts_idx ON emoji_counts (user, emoji)");
     c.execute("CREATE TABLE IF NOT EXISTS message_counts (user TEXT, count INTEGER)");
     c.execute("CREATE UNIQUE INDEX IF NOT EXISTS message_counts_idx ON message_counts (user)");
+    c.execute("CREATE TABLE IF NOT EXISTS message_hashes (user TEXT, message_id TEXT, message_hash TEXT)");
+    c.execute("CREATE UNIQUE INDEX IF NOT EXISTS message_hashes_idx ON message_hashes (message_hash)");
+
+def md5sum(m):
+    return hashlib.md5(m.encode('utf-8')).hexdigest()
 
 conn = sqlite3.connect('persist.sqlite')
+conn.create_function("md5", 1, md5sum)
 start()
 
