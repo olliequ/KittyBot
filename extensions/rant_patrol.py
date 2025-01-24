a91a8b38fec39bb235f5e30a0a3d9b36953b6fb3
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2025-01-25 00:07:53 +1100

    trust it works cbf testing

--- extensions/rant_patrol.py
+++ extensions/rant_patrol.py
@@ -1,34 +1,29 @@
-import os, re
+import re
 import hikari, lightbulb
+import os
 
 plugin = lightbulb.Plugin("rantpatrol")
 
 """
 Bot deletes improperly formatted rants or vents
+Message must start with 'rant: ' or 'vent: ' followed by literally any alphanumeric text. Lowercase only.
 """
 
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
-async def main(event):
-    if event.is_bot or not event.content or "NOTALURKER_ROLE" not in os.environ:
+async def main(event: hikari.GuildMessageCreateEvent):
+    if event.channel_id != os.environ["RANT_AND_VENT_CHANNEL_ID"]:
+        return
+
+    if event.is_bot or not event.content:
         return
 
     messageContent = event.content
-    messageContent = re.sub(r"<.+?>", "", messageContent)
 
-    if not any(c.isalpha() for c in messageContent):
-        return
-
-    if (
-        find_whole_word("payed", messageContent)
-        and not (contains_word(messageContent, "rope"))
-        and not (contains_word(messageContent, "nautical"))
-        and not (contains_word(messageContent, "deck"))
-        and len(messageContent.split()) != 1
-    ):
-        corrected_message = messageContent.replace("payed", "*paid*")
-        output_message = f"""FTFY.\n\nAlthough *payed* exists (the reason why autocorrection didn't help you), it is only correct in:\n\n**1)** Nautical context, when it means to paint a surface, or to cover with something like tar or resin in order to make it waterproof or corrosion-resistant. *The deck is yet to be payed*.\n**2)** Payed out when letting strings, cables or ropes out, by slacking them. *The rope is payed out! You can pull now*.\n\nUnfortunately {event.author.mention}, I was unable to find nautical or rope-related words in your comment."""
-        response = f"> {corrected_message}\n\n{output_message}"
+    if not re.match("^(rant|vent): [a-zA-Z0-9_].*", messageContent):
+        response: str = (
+            f"> Your message must start with 'rant: ' OR 'vent: '. Try again or keep your complaints to yourself."
+        )
         await event.message.respond(response, reply=True, user_mentions=True)
     else:
         return
