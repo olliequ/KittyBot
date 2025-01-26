a9dd39560442b8fd9549f37656bf9413f312b5d8
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2025-01-26 22:57:50 +1100

    fix | fixed rant detection

--- extensions/rant_patrol.py
+++ extensions/rant_patrol.py
@@ -9,6 +9,8 @@ Bot deletes improperly formatted rants or vents
 Message must start with 'rant: ' or 'vent: ' followed by literally any alphanumeric text. Lowercase only.
 """
 
+# helpers
+
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def main(event: hikari.GuildMessageCreateEvent):
@@ -20,13 +22,21 @@ async def main(event: hikari.GuildMessageCreateEvent):
 
     messageContent = event.content
 
-    if not re.match("^(rant|vent): [a-zA-Z0-9_].*", messageContent):
-        response: str = (
-            f"> Your message must start with 'rant: ' OR 'vent: '. Try again or keep your complaints to yourself."
-        )
-        await event.message.respond(response, reply=True, user_mentions=True)
-    else:
-        return
+    """
+    "Rant: too many memes" ==> picked up by kitti
+    "rant: my website's domain got sniped" ==> all good (properly formatted)
+    "shut the fuck up jimmy"" ==> all good (standard comment)    
+    """
+
+    if re.match("^((anti)?(rant|vent):).", messageContent.lower().strip("-")):
+        if not re.match("^((anti-)?(rant|vent):).", messageContent):
+            response: str = (
+                f"> Your message must start with 'rant: ' OR 'vent: ' with the optional 'anti-' prefix. Try again or keep your complaints to yourself."
+            )
+            await event.message.respond(response, reply=True, user_mentions=True)
+        else:
+            return
+    return
 
 
 def load(bot: lightbulb.BotApp):
