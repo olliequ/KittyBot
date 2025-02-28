4d9def4a34aa33028eeb756e2ee2199ce2746a93
Luke Ceddia <luke@flukiluke.com>
2025-02-28 21:30:20 +1100

    Make things work

--- extensions/meme_rater.py
+++ extensions/meme_rater.py
@@ -1,37 +1,44 @@
 import os
+import logging
 import hikari
 import lightbulb
 from PIL import Image
 import requests
-from snark import model
+from extensions.snark import model
 from google.generativeai.generative_models import GenerativeModel
 from typing import Final
 
 plugin = lightbulb.Plugin("MemeRater")
 
-MEMES_CHANNEL_ID = os.environ.get("MEME_CHANNEL_ID")
-
-# can be in .env if you prefer
-MEME_RATE_PROMPT: Final[str] = (
+MEME_CHANNEL_ID = int(os.environ.get("MEME_CHANNEL_ID", 0))
+MEME_RATE_PROMPT: Final[str] = os.environ.get("MEME_RATING_PROMPT",
     "Rate this meme out of 10, with 10 being the funniest. Rate is solely on how funny a bunch of computer science nerds would find it. ONLY Return an integer."
 )
-MINIMUM_MEME_RATING_TO_NOT_DELETE: Final[int] = 6
+MINIMUM_MEME_RATING_TO_NOT_DELETE: Final[int] = int(os.environ.get("MEME_QUALITY_THRESHOLD", "6"))
 DELETE_SHIT: Final[bool] = False
-# removed uncommon formats for testing - don't tell Jimmy plx
-# "tiff",  "bmp", "gif <-- idk if these are supported
 IMG_FILE_EXTENSIONS: Final = {"jpg", "jpeg", "png", "webp"}
 
 
-async def get_meme_rating(image_url: str, model: GenerativeModel):
+def get_meme_rating(image_url: str, model: GenerativeModel):
+    image = requests.get(image_url, stream=True).raw
+    if not image:
+        return ''
     response = model.generate_content(
-        contents=[MEME_RATE_PROMPT, Image.open(requests.get(image_url).raw)]
+        contents=[MEME_RATE_PROMPT, Image.open(image)]
     )
+    logging.info(f'Meme rating response: {response.text}')
     return response.text
 
+def number_emoji(number: int):
+    if number == 10:
+        unicode = "\N{KEYCAP TEN}"
+    else:
+        unicode = f"{number}\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}"
+    return hikari.UnicodeEmoji.parse(unicode)
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def main(event: hikari.GuildMessageCreateEvent) -> None:
-    if event.channel_id != MEMES_CHANNEL_ID:
+    if event.channel_id != MEME_CHANNEL_ID:
         return
 
     ratings = []
@@ -40,7 +47,7 @@ async def main(event: hikari.GuildMessageCreateEvent) -> None:
         if att_ext not in IMG_FILE_EXTENSIONS:
             continue
         image_url = attachment.url
-        rating = await get_meme_rating(image_url, model)
+        rating = get_meme_rating(image_url, model)
         try:
             ratings.append(int(rating))
         except ValueError:
@@ -50,22 +57,18 @@ async def main(event: hikari.GuildMessageCreateEvent) -> None:
 
     avg_rating = min(max(0, sum(ratings) // len(ratings)), 10)
 
-    await event.message.add_reaction(emoji=f":number_{avg_rating}:")
+    await event.message.add_reaction(emoji=number_emoji(avg_rating))
     await event.message.add_reaction(emoji="🐱")
 
     if avg_rating >= MINIMUM_MEME_RATING_TO_NOT_DELETE:
         await event.message.add_reaction(emoji="👍")
-    else:
+    elif DELETE_SHIT:
+        await event.message.delete()
         await event.message.respond(
-            f"This meme is garbage 💩💩💩. I rate it {avg_rating}/10. Send something better.",
-            user_mentions=True,
-            reply=True,
+            f"That meme was garbage 💩💩💩. I rated it {avg_rating}/10. Send something better."
         )
-        if DELETE_SHIT:
-            await event.message.delete()
-        else:
-            await event.message.add_reaction(emoji="💩")
-            await event.message.add_reaction(emoji=f":number_{avg_rating}:")
+    else:
+        await event.message.add_reaction(emoji="💩")
 
 
 def load(bot: lightbulb.BotApp) -> None:
