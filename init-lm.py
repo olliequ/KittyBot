e557f79b08498f7c4150cc81cfba0ede91309aa7
Luke Ceddia <luke@flukiluke.com>
2025-03-25 20:33:44 +1100

    Lint

--- init-lm.py
+++ init-lm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import languagemodels as lm
-lm.config['instruct_model'] = 'Qwen2.5-0.5B-Instruct'
-lm.do('testing')
+
+lm.config["instruct_model"] = "Qwen2.5-0.5B-Instruct"
+lm.do("testing")
